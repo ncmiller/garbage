fdef21c9f3d2f5d6c677412654ac55178dab88bd
Nick Miller <nick@getcruise.com>
2017-12-03 21:12:03 -0800

    day4

--- adventofcode/day4.py
+++ adventofcode/day4.py
@@ -1,8 +1,8 @@
 """read lines from stdin, split lines on whitespace"""
-# import fileinput
-# lines = []
-# for line in fileinput.input():
-#     lines.append([int(x) for x in line.split()])
+import fileinput
+lines = []
+for line in fileinput.input():
+    lines.append([x for x in line.split()])
 
 
 """read single number from stdin"""
@@ -15,4 +15,31 @@
 #             if ch != '\n':
 #                 print ch
 
+""" part1 """
+valid = 0
+for line in lines:
+    d = {}
+    is_valid = True
+    for word in line:
+        if word in d:
+            is_valid = False
+            break
+        d[word] = 1
+    if is_valid:
+        valid += 1
+print valid
 
+""" part2 """
+valid = 0
+for line in lines:
+    d = {}
+    is_valid = True
+    for word in line:
+        w = ''.join(sorted(word))
+        if w in d:
+            is_valid = False
+            break
+        d[w] = 1
+    if is_valid:
+        valid += 1
+print valid
