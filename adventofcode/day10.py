d46c1fe8552b8938edc6da5c2cd283ee7e9365d7
Nick Miller <nick@getcruise.com>
2017-12-09 21:48:24 -0800

    day10

--- adventofcode/day10.py
+++ adventofcode/day10.py
@@ -1,15 +1,57 @@
-"""read lines from stdin, split lines on whitespace"""
-# import fileinput
-# lines = []
-# for line in fileinput.input():
-#     lines.append([int(x) for x in line.split()])
+import re
 
-"""read one number per line"""
-# import fileinput
-# lines = []
-# for line in fileinput.input():
-#     lines.append(int(line))
+puzzle = raw_input()
+lengths = map(int, puzzle.split(','))
+N = 256
+l = range(N)
 
-"""read all characters in file"""
-# with open("dayX_input.txt") as f:
-#     chars = f.read()
+def reverse_slice(pos, length):
+    n = pos + length
+    if n >= N:
+        forward_slice = l[pos:] + l[:(n%N)]
+        rev_slice = list(reversed(forward_slice))
+        l[pos:] = rev_slice[:N-pos]
+        l[:n%N] = rev_slice[N-pos:]
+    else:
+        forward_slice = l[pos:n]
+        l[pos:n] = list(reversed(forward_slice))
+
+pos = 0
+skip = 0
+for length in lengths:
+    reverse_slice(pos, length)
+    pos = (pos + length + skip) % N
+    skip += 1
+
+print l[0]*l[1]
+
+#----
+
+lengths = puzzle.lstrip().rstrip()
+def to_hex(v):
+    return "{:02x}".format(v)
+
+l = range(N)
+nrounds = 64
+a = map(lambda x: ord(x), lengths)
+a = a + [17, 31, 73, 47, 23]
+
+pos = 0
+skip = 0
+for i in range(64):
+    for length in a:
+        reverse_slice(pos, length)
+        pos = (pos + length + skip) % N
+        skip += 1
+sparse_hash = l
+
+dense_hash = []
+
+for i in range(16):
+    x = 0
+    vals = l[(i*16):(i*16+16)]
+    for v in vals:
+        x = x ^ v
+    dense_hash.append(x)
+
+print ''.join(map(to_hex, dense_hash))
---
+++ adventofcode/day11.py
@@ -0,0 +1,16 @@
+"""read lines from stdin, split lines on whitespace"""
+# import fileinput
+# lines = []
+# for line in fileinput.input():
+#     lines.append([int(x) for x in line.split()])
+
+"""read one number per line"""
+# import fileinput
+# lines = []
+# for line in fileinput.input():
+#     lines.append(int(line))
+
+"""read all characters in file"""
+# with open("dayX_input.txt") as f:
+#     chars = f.read()
+
