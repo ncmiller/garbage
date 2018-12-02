35cf4eb0efabde9b333aae64c0428716d04fcb02
Nick Miller <nick@getcruise.com>
2018-12-02 09:03:49 -0800

    day2

--- adventofcode/2018/day2.py
+++ adventofcode/2018/day2.py
@@ -1,4 +1,5 @@
 import fileinput
+from collections import Counter
 #----------------------
 # File IO
 #----------------------
@@ -12,3 +13,33 @@ import fileinput
 # weight = int(fields[1].replace('(','').replace(')',''))
 # stowers = [x.split(',')[0] for x in fields[2:]]
 #----------------------
+
+lines = [l.rstrip() for l in fileinput.input()]
+
+def part1():
+    twos = set()
+    threes = set()
+    for l in lines:
+        counts = Counter(l)
+        for k,v in counts.most_common():
+            if v == 2:
+                twos.add(l)
+            if v == 3:
+                threes.add(l)
+    print len(twos) * len(threes)
+
+def part2():
+    slines = sorted(lines)
+    for i in range(0,len(slines)-1):
+        a = slines[i]
+        b = slines[i+1]
+        matching = []
+        for j in range(len(a)): # assume same len for both
+            if a[j] == b[j]:
+                matching.append(a[j])
+        if len(matching) == len(a) - 1:
+            print ''.join(matching)
+            return
+
+part1()
+part2()
