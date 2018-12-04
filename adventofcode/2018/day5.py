e199867a08f3b254190c90ce77d703e1d415d2cf
Nick Miller <nick@getcruise.com>
2018-12-04 08:13:12 -0800

    day4

---
+++ adventofcode/2018/day5.py
@@ -0,0 +1,14 @@
+import fileinput
+#----------------------
+# File IO
+#----------------------
+# for line in fileinput.input():
+
+# with open("day9_input.txt") as f:
+#     chars = f.read()
+
+# vals = map(int, fileinput.input())
+
+# weight = int(fields[1].replace('(','').replace(')',''))
+# stowers = [x.split(',')[0] for x in fields[2:]]
+#----------------------
