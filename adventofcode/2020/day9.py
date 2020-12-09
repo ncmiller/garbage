0450ef4da05b1bfe775ebd1e191c3fdbc22228a2
Nick Miller <nick.miller@spindance.com>
2020-12-09 07:53:57 -0800

    AoC 2020, day 9

--- adventofcode/2020/day9.py
+++ adventofcode/2020/day9.py
@@ -5,15 +5,35 @@ def parse():
     vals = []
     for line in fileinput.input():
         l = line.rstrip()
-        op,num = l.split(' ')
-        num = int(num)
-        vals.append((op,num))
+        vals.append(int(l))
     return vals
 
+def find_sum(n, vals):
+    for i in range(len(vals)):
+        for j in range(i+1, len(vals)):
+            if (vals[i]+vals[j]) == n:
+                return True
+    return False
+
 def part1(vals):
+    preamble_len = 25
+    for i in range(preamble_len, len(vals)):
+        prior = vals[i-preamble_len:i]
+        n = vals[i]
+        if not find_sum(n, prior):
+            return n
     return 0
 
 def part2(vals):
+    inval_num = part1(vals)
+    for i in range(len(vals)):
+        for j in range(i+2, len(vals)):
+            r = vals[i:j]
+            s = sum(r)
+            if s == inval_num:
+                return min(r) + max(r)
+            elif s > inval_num:
+                break
     return 0
 
 vals = parse()
