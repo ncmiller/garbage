dd3b433edd8a40f1bd02dcb74fdcb13224fcb197
Nick Miller <nick.miller@spindance.com>
2020-12-15 14:40:56 -0800

    AoC 2020, day 14

--- adventofcode/2020/day14.py
+++ adventofcode/2020/day14.py
@@ -8,12 +8,88 @@ def parse():
         vals.append(l)
     return vals
 
-def part1(vals):
-    return 0
+def apply_mask(mask, val):
+    newval = 0
+    lenm = len(mask)
+    for i in range(lenm):
+        bitpos = lenm - i - 1
+        valbit = (1 << bitpos) & val
+        if mask[i] == '0':
+            pass
+        elif mask[i] == '1':
+            newval += (1 << bitpos)
+        else:
+            newval += valbit
+    # print(val, mask, newval)
+    return newval
+
+def part1(lines):
+    mem = {}
+    mask = ''
+    for l in lines:
+        if l[:3] == 'mas':
+            mask = l[7:]
+        else:
+            words = l.split(' ')
+            addr = int(words[0][4:-1])
+            val = int(words[2])
+            val = apply_mask(mask, val)
+            mem[addr] = val
+    return sum([v for v in mem.values()])
+
+def expand_addr(addr, val, num_x):
+    eaddr = ''
+    lena = len(addr)
+    for i in range(lena):
+        if addr[i] == 'X':
+            valbit = (1 << num_x-1) & val
+            if valbit:
+                eaddr += '1'
+            else:
+                eaddr += '0'
+            num_x -= 1
+        else:
+            eaddr += addr[i]
+    return int(eaddr,2)
+
+def get_addrs(addr, mask):
+    addrs = []
+    lenm = len(mask)
+    dest_addr = ''
+    for i in range(lenm):
+        bitpos = lenm - i - 1
+        if mask[i] == '0':
+            addrbit = (1 << bitpos) & addr
+            if addrbit:
+                dest_addr += '1'
+            else:
+                dest_addr += '0'
+        elif mask[i] == '1':
+            dest_addr += '1'
+        else:
+            dest_addr += 'X'
+    num_x = sum([1 for c in dest_addr if c == 'X'])
+    for i in range(2**num_x):
+        addrs.append(expand_addr(dest_addr,i, num_x))
+    # print(addrs)
+    return addrs
 
 def part2(vals):
-    return 0
+    mem = {}
+    mask = ''
+    for l in lines:
+        if l[:3] == 'mas':
+            mask = l[7:]
+        else:
+            words = l.split(' ')
+            addr = int(words[0][4:-1])
+            val = int(words[2])
+            addrs = get_addrs(addr, mask)
+            for a in addrs:
+                mem[a] = val
+    return sum([v for v in mem.values()])
 
-vals = parse()
-print(part1(list(vals)))
-print(part2(list(vals)))
+# too low: 6960776365110
+lines = parse()
+print(part1(list(lines)))
+print(part2(list(lines)))
