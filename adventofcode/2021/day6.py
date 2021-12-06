e79a1b9c2fb63ae6d0f00841c7dc53576c0f8a62
Nick Miller <nick.miller@spindance.com>
2021-12-06 10:47:05 -0800

    AoC 2021, Day6

--- adventofcode/2021/day6.py
+++ adventofcode/2021/day6.py
@@ -1,14 +1,41 @@
 import fileinput
 import copy
 
-def part1(lines):
-    pass
+def part1(fish, ndays=80):
+    for day in range(ndays):
+        # new_fish = []
+        for i in range(len(fish)):
+            if fish[i] == 0:
+                fish[i] = 6
+                fish.append(8)
+            else:
+                fish[i] -= 1
+        # print(day, len(fish))
+    return len(fish)
 
-def part2(lines):
-    pass
+memo = {}
+def part2(fish, ndays=256):
+    if ndays in memo:
+        return memo[ndays]
 
-lines = list(fileinput.input())
-print(part1(copy.deepcopy(lines)))
-print(part2(copy.deepcopy(lines)))
+    total = 0
+    new_fish = []
+    for i in range(fish[0], ndays, 7):
+        total += 1
+        total += part2([8], ndays - i - 1)
+    # print(ndays, total)
+    memo[ndays] = total
+    return total
 
+line = list(fileinput.input())[0]
+fish = list(map(int, line.split(',')))
+print(part1(copy.deepcopy(fish), 80))
+
+# part2 wrapper
+total = len(fish)
+for i in range(len(fish)):
+    memo = {}
+    ans = part2(copy.deepcopy([fish[i]]), 256)
+    total += ans
+print(total)
 
---
+++ adventofcode/2021/day7.py
@@ -0,0 +1,12 @@
+import fileinput
+import copy
+
+def part1(lines):
+    pass
+
+def part2(lines):
+    pass
+
+lines = list(fileinput.input())
+print(part1(copy.deepcopy(lines)))
+print(part2(copy.deepcopy(lines)))
